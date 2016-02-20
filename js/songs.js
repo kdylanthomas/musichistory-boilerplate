0ffdea1ca8f6a1824b1489afcd09eb9a04d9ec23
Dylan Thomas <dylanthomas@Dylans-MacBook-Pro-2.local>
2016-02-20 15:13:20 -0600

    dynamic dropdown values added, working on filter btn

--- js/songs.js
+++ js/songs.js
@@ -6,7 +6,21 @@ $.ajax({
 
 function sendToOutput(songData) {
 	let songs = songData.songs;
+	createUserSelects(songs);
 	outputSongs(songs);
+}
+
+// function that dynamically adds artists, albums to dropdowns
+function createUserSelects(songs) {
+	$(songs).each((i, currSong) => {
+		// if you can't find an option el with the artistID on it, make a new option el
+		if (!$("#artist-dropdown").find("#"+currSong.artist_id).length) {
+			$("#artist-dropdown").append(`<option id=${currSong.artist_id} val=${currSong.artist_id}>${currSong.artist}</option>`);
+		}
+		if (!$("#album-dropdown").find("#"+currSong.album_id).length) {
+			$("#album-dropdown").append(`<option id=${currSong.album_id} val=${currSong.album_id}>${currSong.album}</option>`);
+		}
+	});
 }
 
 // output to DOM for loaded & manually added songs
@@ -15,8 +29,8 @@ function outputSongs(songs) {
 		$('#right-side').append(
 			`<div class="song-list">
 				<h5>${currSong.title}</h5>
-				<span>${currSong.artist}</span> -
-				<span>${currSong.album}</span> -
+				<span class="list-artist">${currSong.artist}</span> -
+				<span class="list-album">${currSong.album}</span> -
 				<span>${currSong.genre}</span>
 				<button class="delete-single">Delete</button>
 			</div>`
@@ -45,10 +59,13 @@ $('#add-song').on('click', function() {
 		"title": $('#song').val(),
 		"artist": $('#artist').val(),
 		"album": $('#album').val(),
-		"genre": $('#genre :selected').text()
+		"genre": $('#genre :selected').text(),
+		"artist_id": $('#artist').val().substring(0,3),
+		"album_id": $('#album').val().substring(0,3)
 	};
 	console.log(newSong);
 	if (newSong.title && newSong.artist && newSong.album) {
+		createUserSelects(newSong);
 		outputSongs(newSong);
 		$('#song').val('');
 		$('#artist').val('');
@@ -61,12 +78,20 @@ $('#add-song').on('click', function() {
 
 $('.textbox-input').on('keyup', function() {
 	$('#success-msg').html('');
-})
+});
 
 // delete & more songs button functionality (event bubbling)
 $('#right-side').on('click', function(e) {
 	if ($(e.target).hasClass('delete-single')) {
+		// need to remove artist and album from options,
+		// but only if it isn't still needed for another song (in progress)
+		let artist = $(e.target).parent().find('.list-artist').html();
+		// this if statement removes all options from dropdown...why?
+		// if ($('#artist-dropdown').has('option').html(artist)) {
+			console.log('artist', artist);
+		// }
 		$(e.target).parent().remove();
+
 	}
 	if (e.target.id === "more-songs") {
 		console.log('more songs');
@@ -76,3 +101,10 @@ $('#right-side').on('click', function(e) {
 	}
 });
 
+// filter button functionality
+$('#filter').on('click', function() {
+	console.log($('#artist-dropdown :selected').val());
+	console.log($('#album-dropdown :selected').val());
+	// not sure what this is doing
+	console.log($('input:checkbox').prop('checked'));
+})
